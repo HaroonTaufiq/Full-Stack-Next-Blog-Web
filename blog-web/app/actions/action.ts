5257d623da233d803abf0479676c4f0de78cb23f
Haroon <haroon.taufiq@ccript.com>
2025-01-29 02:01:45 +0500

    connecting prisma SQLite DB

--- blog-web/app/actions/action.ts
+++ blog-web/app/actions/action.ts
@@ -1,7 +1,15 @@
 'use server'
 
+import prisma from "@/lib/db"
+
 export async function createPost (formData: FormData){
-    const title = formData.get('title')?.toString()
-    const content = formData.get('content')?.toString()
-    console.log(title)
+    const title = formData.get('title') as string
+    const content = formData.get('content') as string
+    
+    await prisma.post.create({
+        data: {
+            title,
+            content,
+        }
+    })
 }
